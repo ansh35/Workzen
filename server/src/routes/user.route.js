c0034cc7f0664a551eb6b9c370b17233c1f0974f
ansh35 <khareansh075@gmail.com>
2026-03-11 01:35:33 +0530

    chore: bug fixes

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { getAllUsers } = require("../controllers/user");
-const authMiddleware = require("../middleware/middleware");
-const roleMiddleware = require("../middleware/role");
+const authMiddleware = require("../Middleware/middleware");
+const roleMiddleware = require("../Middleware/role");
 
 // ✅ Admin only
 router.get(
